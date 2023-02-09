c2585975b509b908c41406bd1926b8b29490c434
WJ_Kim <mw7895la@gmail.com>
2023-02-09 23:38:15 +0900

    [2023-02-09] SpringMVC #2 1. fragment, insert, replace 2. Unescaped 3. block

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -103,14 +103,44 @@ public class BasicController {
         return "basic/each";
     }
 
-    private void addUsers(Model model){
+
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+
+        Object users = model.getAttribute("users");
+        System.out.println(model);
+
+        return "basic/javascript";
+    }
+
+    private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
         list.add(new User("UserB", 20));
         list.add(new User("UserC", 30));
 
-        model.addAttribute("users",list);
-
+        model.addAttribute("users", list);
     }
 
 
